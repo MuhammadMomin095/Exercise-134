61cc98f0dbe7120f2f9f8b376dbdd2c603fad88f
Muhammad Momin <160387789+MuhammadMomin095@users.noreply.github.com>
2024-04-17 14:40:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+var jsonString = '{"name":"Alice","age":30,"city":"Wonderland"}';
+var person = JSON.parse(jsonString);
+console.log(person);
---
+++ app.ts
@@ -0,0 +1,6 @@
+const jsonString = '{"name":"Alice","age":30,"city":"Wonderland"}';
+
+const person = JSON.parse(jsonString);
+
+
+console.log(person);
